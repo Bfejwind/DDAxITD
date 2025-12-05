74102d386ec08170fb23941a9960a96a630f8ade
bfejwndbdu <officiallyrichard@gmail.com>
2025-12-06 02:28:19 +0800

    Login & Register Menu
    swapping between login and register works, needs DDA and error management

---
+++ Assets/Scripts/UIChange.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class UIChange : MonoBehaviour
+{
+    public GameObject SelectUI;
+    public GameObject ErrorUser;
+    public GameObject ErrorPass;
+    public GameObject RegisterUI;
+    public GameObject LoginUI;
+    void Awake()
+    {
+        SelectUI.SetActive(false);
+        ErrorUser.SetActive(false);
+        ErrorPass.SetActive(false);
+        RegisterUI.SetActive(false);
+    }
+    // public void ActivateRegister()
+    // {
+    //     LoginUI.SetActive(false);
+    //     RegisterUI.SetActive(true);
+    // }
+    // public void ActivateLogin()
+    // {
+    //     RegisterUI.SetActive(false);
+    //     LoginUI.SetActive(true);
+        
+    // }
+    
+}
