7f786ad0f8502fa8c8b33842af0d2a6b0d3da401
bfejwndbdu <officiallyrichard@gmail.com>
2025-12-09 14:23:43 +0800

    Repling Archetypes Focus
    Change was done to UIImage Switcher

--- Assets/Scripts/UIImageSwitcher.cs
+++ Assets/Scripts/UIImageSwitcher.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,12 +8,20 @@ public class UIImageSwitcher : MonoBehaviour
     public Image displayImage;
     public Sprite[] images;
     public int currentIndex = 0;
+    public List<string> archetypes = new List<string>();
+    public TMP_Text archetypeStat;
+    void Start()
+    {
+        archetypes.Add("Strength");
+        archetypes.Add("Speed");
+    }
 
     public void NextImage()
     {
         currentIndex++;
         if (currentIndex >= images.Length) currentIndex = 0;
         displayImage.sprite = images[currentIndex];
+        archetypeStat.text = archetypes[currentIndex];
     }
 
     public void PreviousImage()
@@ -19,5 +29,6 @@ public class UIImageSwitcher : MonoBehaviour
         currentIndex--;
         if (currentIndex < 0) currentIndex = images.Length - 1;
         displayImage.sprite = images[currentIndex];
+        archetypeStat.text = archetypes[currentIndex];
     }
 }
