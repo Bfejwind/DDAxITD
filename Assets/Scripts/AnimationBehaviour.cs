1822784e3c39f3e8a37e2ae6caafd0d8a3a65b79
bfejwndbdu <officiallyrichard@gmail.com>
2025-12-13 14:29:10 +0800

    Fox Animations+Model spawn
    Fox animations. Models spawn based on your repling choice.

--- Assets/Scripts/AnimationBehaviour.cs
+++ Assets/Scripts/AnimationBehaviour.cs
@@ -4,6 +4,8 @@ public class AnimationBehaviour : MonoBehaviour
 {
     [SerializeField]
     Animator SBearAnim;
+    [SerializeField]
+    Animator SFoxAnim;
     public int animationIndex;
     public void SDecideAnimation()
     {
@@ -23,6 +25,26 @@ public class AnimationBehaviour : MonoBehaviour
         else
         {
             SBearAnim.SetTrigger("Tapped");    
+        }
+    }
+    public void SFoxDecideAnimation()
+    {
+        animationIndex = Random.Range(1,21);
+        if (animationIndex == 1)
+        {
+            SFoxAnim.SetTrigger("Deadge");
+            GameManagerScript.Instance.repsDone--;
+            GameManagerScript.Instance.ModifyReps();
+        }
+        else if (animationIndex == 20)
+        {
+            SFoxAnim.SetTrigger("Crit");
+            GameManagerScript.Instance.repsDone++;
+            GameManagerScript.Instance.ModifyReps();
+        }
+        else
+        {
+            SFoxAnim.SetTrigger("Tapped");    
         }
     }
 }
